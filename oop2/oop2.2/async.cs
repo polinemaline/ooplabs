c06b3438ea181a4dacf0b27c4a3ed853b8322f6c
polinemaline <146850351+polinemaline@users.noreply.github.com>
2024-05-08 15:26:26 +0300

    Add files via upload

---
+++ oop2/oop2.2/async.cs
@@ -0,0 +1,44 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace oop2._2
+{
+    class Request
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var urls = new[] { "https://edu.stankin.ru/", "https://www.kinopoisk.ru/", "https://github.com/" };
+
+            var startTime = DateTime.Now;
+
+            var tasks = new Task[urls.Length];
+            for (int i = 0; i < urls.Length; i++)
+            {
+                tasks[i] = GetJSONAsync(client, urls[i], i);
+            }
+
+            await Task.WhenAll(tasks);
+
+            var endTime = DateTime.Now;
+            Console.WriteLine($"Total time: {endTime - startTime}");
+            Console.ReadKey();
+        }
+        static async Task GetJSONAsync(HttpClient client, string url, int i)
+        {
+            var response = await client.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine($"URL{i + 1}: Error: {response.StatusCode}");
+            }
+            else
+            {
+                var JSON = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"URL{i + 1}: {JSON}");
+            }
+           
+        }
+    }
+}
---
+++ oop2/sync.cs
@@ -0,0 +1,65 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace oop2
+//{
+
+//    internal class Program
+//    {
+
+//        async static Task Main(string[] args)
+//        {
+//            await PrintAsync();   // вызов асинхронного метода
+//            void Print()
+//            {
+//                Thread.Sleep(3000);     // имитация продолжительной работы
+//                Console.WriteLine("Hello METANIT.COM");
+//            }
+
+//            // определение асинхронного метода
+//            async Task PrintAsync()
+//            {
+//                Console.WriteLine("Начало метода PrintAsync"); // выполняется синхронно
+//                await Task.Run(() => Print());                // выполняется асинхронно
+//                Console.WriteLine("Конец метода PrintAsync");
+//            }
+//            Console.ReadKey();
+//        }
+//    }
+//}
+using System;
+using System.Net.Http;
+
+namespace oop2
+{
+    class Request
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            var urls = new[] { "https://edu.stankin.ru/", "https://www.kinopoisk.ru/", "https://github.com/" };
+
+            var startTime = DateTime.Now;
+            for (int i = 0; i < urls.Length; i++)
+            {
+                var response = client.GetAsync(urls[i]).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"URL{i + 1}: Error: {response.StatusCode}");
+                }
+                else
+                {
+                    var JSON = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine($"URL{i + 1}: {JSON}");
+                }
+            }
+            var endTime = DateTime.Now;
+            Console.WriteLine($"Total time: {endTime - startTime}");
+            Console.ReadKey();
+        }
+    }
+}
