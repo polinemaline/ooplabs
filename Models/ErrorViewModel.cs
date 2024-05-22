9525c0d2deda3dd3388c1bfc27a3fcc63e64d93d
polinemaline <polinemaline34@yandex.ru>
2024-05-22 16:02:24 +0300

    delete

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace oop3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
